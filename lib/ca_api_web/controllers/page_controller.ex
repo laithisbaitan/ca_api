925afca84a94874976f0e878340f8b1fc0e8b126
laithisbaitan <80163581+laithisbaitan@users.noreply.github.com>
2024-10-10 10:13:30 +0300

    added ui at /
    you can interact with search api by using the route "/api/search"
    or ui at route "/"

--- lib/ca_api_web/controllers/page_controller.ex
+++ lib/ca_api_web/controllers/page_controller.ex
@@ -2,8 +2,45 @@ defmodule CaApiWeb.PageController do
   use CaApiWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     render(conn, :home, layout: false)
+  end
+
+  def submit(conn, params) do
+    # Create query string
+    query = %{"query" => [
+      "OBJ_HODCODE:#{params["hod_code"]}",
+      "OBJ_HAYCODE:#{params["hay_code"]}",
+      "OBJ_PIECECODE:#{params["piece_code"]}"
+    ]}
+
+    case HTTPoison.post("localhost:4000/api/query", Jason.encode!(query), [{"Content-Type", "application/json"}]) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.inspect(conn)
+        body = Jason.decode!(body)["results"]
+        conn
+        |> put_status(:ok)
+        |> render(:results, results: body)
+
+      # {:ok, %HTTPoison.Response{status_code: :bad_request, body: body}} ->
+      #   conn
+      #   |> put_status(:bad_request)
+      #   |> json(%{error: "Request failed", response: Jason.decode!(body)})
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        conn
+        |> put_status(:internal_server_error)
+        |> json(%{error: "Request failed", reason: reason})
+    end
+
+    # # Call API with query
+    # case CaApiWeb.CollectiveAccessController.query_search(conn, %{"query" => query}) do
+    #   %{"ok" => true, "results" => results} ->
+    #     render(conn, "results.html", results: results)
+
+    #   %{"error" => reason} ->
+    #     conn
+    #     |> put_flash(:error, "Search failed: #{reason}")
+    #     |> redirect(to: "/")
+    # end
   end
 end
--- lib/ca_api_web/router.ex
+++ lib/ca_api_web/router.ex
@@ -6,7 +6,7 @@ defmodule CaApiWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, html: {CaApiWeb.Layouts, :root}
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -18,6 +18,7 @@ defmodule CaApiWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    post "/submit", PageController, :submit
   end
 
   scope "/api", CaApiWeb do
